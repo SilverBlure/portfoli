795b46470dee8b1ca3c4995f43625682a4660c62
SilverBlure <oesterle.ni@onlinehome.de>
2025-11-24 17:15:14 +0100

    adding content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from "./hero-section/hero-section.component";
-
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { WhymeComponent } from "./whyme/whyme.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent],
+  imports: [RouterOutlet, HeroSectionComponent, NavbarComponent, WhymeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
