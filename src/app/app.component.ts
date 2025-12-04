121825f739da07502e7295adf953855b1c00f179
Nicolai Österle <oesterle.ni@onlinehome.de>
2025-12-04 19:16:58 +0100

    adding content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from "./hero-section/hero-section.component";
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { WhymeComponent } from "./whyme/whyme.component";
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent, NavbarComponent, WhymeComponent],
+  imports: [RouterOutlet, HeroSectionComponent, NavbarComponent, WhymeComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
