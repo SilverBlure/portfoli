b0bc11a1b8a73c38a374252c6c06f5d45b6f02e9
SilverBlure <76730862+SilverBlure@users.noreply.github.com>
2026-01-27 19:50:58 +0100

    add my projects

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
+import { SharkyComponent } from './sharky/sharky.component';
+import { JoinComponent } from './join/join.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-projects',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule,CommonModule, SharkyComponent, JoinComponent, PokemonComponent],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
 export class MyProjectsComponent {
 
+
+     projects = [{title: 'Join', component: JoinComponent },
+      {title: 'Pokemon', component: PokemonComponent },
+      {title: 'Sharky', component: SharkyComponent },
+      ];
 }
