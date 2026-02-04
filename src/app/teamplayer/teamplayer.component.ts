19799803fe02190eefcddcae8d44c0304d753fc4
SilverBlure <76730862+SilverBlure@users.noreply.github.com>
2026-02-04 16:13:35 +0100

    add contact section

--- src/app/teamplayer/teamplayer.component.ts
+++ src/app/teamplayer/teamplayer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-teamplayer',
   standalone: true,
-  imports: [],
+  imports: [ContactComponent],
   templateUrl: './teamplayer.component.html',
   styleUrl: './teamplayer.component.scss'
 })
