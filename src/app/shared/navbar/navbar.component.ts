5b78fa82bdcc6aaea7e8815c2d51b218a93c2d37
SilverBlure <76730862+SilverBlure@users.noreply.github.com>
2026-02-11 16:53:42 +0100

    add ger eng background

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
-
-  languageButton :  'eng' | 'ger' = 'ger';
-
+  state: 'eng' | 'ger' = 'eng'
 
 
+    setLanguageState(pagestate: 'eng' | 'ger'){
+      if( pagestate !=  this.state){
+        this.state = pagestate
+      }
+    }
 
-
-
-
-  
 }
